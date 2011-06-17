81d88bc4dc3c54de97b2f5f874806871bc251b53
Kristian Øllegaard <kristian@oellegaard.com>
2011-06-17 19:09:37 +0200

    updated requirements

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 from setuptools import setup, find_packages
 
-version = 'Unknown'
+version = '0.1'
 
 setup(
     name = 'cmsplugin-flickr',
     version = version,
     description = 'flickr plugin for django-cms',
-    author = 'Unknown',
+    author = 'Bernhard Vallant',
     author_email = '',
     packages = find_packages(),
     zip_safe=False,
     install_requires=[
         'Django>=1.2',
         'django-cms',
+		'flickrapi',
     ],
 )

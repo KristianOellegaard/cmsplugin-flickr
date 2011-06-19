fad0f8133114fd841f97d08e0632c5b15908d727
Kristian Øllegaard <kristian@oellegaard.com>
2011-06-19 13:37:33 +0200

    Updated setup.py, to include templates too

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     author_email = '',
     packages = find_packages(),
     zip_safe=False,
+    include_package_data = True,
     install_requires=[
         'Django>=1.2',
         'django-cms',
